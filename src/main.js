ff8bcf78109a485a0d18d8011c47a4ecaab674f6
micaelsgarcez <99693843+micaelsgarcez@users.noreply.github.com>
2022-05-14 10:20:12 -0300

    Update responsivity

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
 import './index.css'
+
+window.onload = () => {
+  const btnOpenMenu = document.querySelector('.btn-open-menu')
+  const btnCloseMenu = document.querySelector('.btn-close-menu')
+  const menu = document.querySelector('.menu')
+
+  btnOpenMenu.onclick = () => {
+    menu.classList.add('open-menu')
+  }
+
+  btnCloseMenu.onclick = () => {
+    menu.classList.remove('open-menu')
+  }
+
+  const menuLinks = document.querySelectorAll('.menu a')
+  menuLinks.forEach((_item, index) => {
+    menuLinks[index].onclick = (e) => {
+      e.preventDefault()
+      menu.classList.remove('open-menu')
+    }
+  })
+}
